e096ae25c39a8d00dbb99dc072d864d69ba8bf35
ys <a123@chalinandeMac-mini.local>
2017-09-24 21:10:52 +0800

    add hostname

--- config.py
+++ config.py
@@ -1,7 +1,20 @@
+# _*_coding:utf8 _*_
 import os
+import socket
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 #SQLALCHEMY_DATABASE_URI = 'mysql://root:1234pttK@47.93.103.136:3306/test?charset=utf8'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1234pttK@127.0.0.1:3306/test?charset=utf8'
+
+hostname = socket.gethostname()
+if hostname == 'chalinandeMac-mini.local': #开发环境
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:@127.0.0.1:3306/test?charset=utf8'
+
+if hostname == 'iZ2ze7ry8wsws4xlrloit3Z':  #生产环境
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:1234pttK@127.0.0.1:3306/test?charset=utf8'
+
+
+
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
